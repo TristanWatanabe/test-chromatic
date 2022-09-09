7bd21bb7704b9966698433170233cc099669fc89
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-09-09 17:03:56 -0400

    test 3 (#5)

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -19,7 +19,7 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
-  label: 'Test 2',
+  label: 'Test 3 ',
 };
 
 export const Secondary = Template.bind({});
