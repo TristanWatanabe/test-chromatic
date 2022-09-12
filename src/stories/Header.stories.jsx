7687e282f82a7bd1cf9316542a2d758831d933cc
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-09-12 07:02:58 -0700

    use storiesOf and addStory

--- src/stories/Header.stories.jsx
+++ src/stories/Header.stories.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
+import { storiesOf } from '@storybook/react';
 
 import { Header } from './Header';
 
-export default {
-  title: 'Example/Header',
-  component: Header,
-  parameters: {
-    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
-  },
-};
+// export default {
+//   title: 'Example/Header',
+//   component: Header,
+//   parameters: {
+//     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+//     layout: 'fullscreen',
+//   },
+// };
 
-const Template = (args) => <Header {...args} />;
+// const Template = (args) => <Header {...args} />;
 
-export const LoggedIn = Template.bind({});
-LoggedIn.args = {
-  user: {
-    name: 'Jane Doe',
-  },
-};
+// export const LoggedIn = Template.bind({});
+// LoggedIn.args = {
+//   user: {
+//     name: 'Jane Doe',
+//   },
+// };
 
-export const LoggedOut = Template.bind({});
-LoggedOut.args = {};
+// export const LoggedOut = Template.bind({});
+// LoggedOut.args = {};
+
+
+storiesOf('Header', module)
+.addStory('Logged In ', () => {
+  <Header user={{name: 'Jane Doe'}} />
+})
+.addStory('Logged Out ', () => {
+  <Header user={{}} />
+})
