d43dd47faceac212f7eccc39267ba57bd655f407
Tristan Watanabe <tristan.watanabe@gmail.com>
2022-09-22 10:23:36 -0700

    test

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -19,7 +19,7 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
-  label: 'Parallel test 2',
+  label: 'Parallel test 2.2',
 };
 
 export const Secondary = Template.bind({});
